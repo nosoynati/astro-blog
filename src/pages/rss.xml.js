1899ca85ba79aefed5eab907240267cf31254fe5
Natasha <shotaoouji@gmail.com>
2026-01-23 08:01:49 -0300

    fix tag paeg

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,28 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss'; -> for pages
+import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+// export async function GET(context) {
+//   return rss({
+//     title: "Astro Learner | Blog",
+//     description: "My journmey learningAstro",
+//     site: context.site,
+//     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//     customData: `<language>es-la</language>`
+//   });
+// }
+export async function GET(context){
+  const posts = await getCollection('blog');
   return rss({
     title: "Astro Learner | Blog",
     description: "My journmey learningAstro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>es-la</language>`
-  });
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}`,
+    })),
+    customData: `<language>es-ar</language>`
+  })
 }
